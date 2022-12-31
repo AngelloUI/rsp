090c1a4c18f6d666c418b36a85d10e5d5dac667c
Dmitry <usefullforit@gmail.com>
2022-12-31 17:39:42 +0300

    php -> html

--- input-page.php
+++ input-page.php
@@ -3,15 +3,14 @@ $players_amount = null;
 $bots_amount = null;
 $input_params = null;
 if (isset($_POST['input_params']) && isset($_POST['bots_amount']) && isset($_POST['players_amount'])) {
-    $input_params = trim($_POST['input_params']," ");
+    $input_params = trim($_POST['input_params'], " ");
     $input_params = explode(" ", $input_params);
-    if (count($input_params) % 2 == 0){
+    if (count($input_params) % 2 == 0) {
         echo "amount of your params is even. Correct it";
-    }else{
+    } else {
         $bots_amount = (int)($_POST['bots_amount']);
         $players_amount = (int)($_POST['players_amount']);
-
     }
 } else {
-    echo "wrong input data",'<br>';
+    echo "wrong input data", '<br>';
 }
